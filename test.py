5a2a819dcb29827c80cc2d5da10af3e231eed90f
pedro <pgcotovio@gmail.com>
2021-05-04 18:45:39 +0100

    refactor testing

--- test.py
+++ test.py
@@ -1,20 +1,48 @@
 import click
 import nclustgen
 import numpy
+from scipy.sparse import csr_matrix
+from sparse import COO
 
 
 @click.command()
 @click.option('--kind', default='TriclusterGenerator', help='Type of test')
+@click.option('--memory', default=False, help='If in memory')
 @click.option('--save', default=False, help='If save')
-def test(kind, save):
+@click.option('--ifreturn', default=False, help='to omit return')
+def testcli(kind, save, memory, ifreturn):
+
+    test(kind, save, memory, ifreturn)
+
+
+def test(kind='TriclusterGenerator', save=False, memory=True, ifreturn=True):
 
     cl = getattr(nclustgen, kind)()
-    cl.generate()
-    #assert isinstance(cl.generate(), numpy.ndarray)
+    x, y = cl.generate(in_memory=memory)
+
+    assert isinstance(y, list)
+
+    if memory:
+        assert isinstance(x, numpy.ndarray)
+
+    else:
+        assert isinstance(x, csr_matrix) or isinstance(x, COO)
+
+    if kind == 'TriclusterGenerator':
+
+        if x.shape != (3, 100, 100):
+            raise AssertionError('wrong shape ... {}'.format(x.shape))
+
+    else:
+        if x.shape != (100, 100):
+            raise AssertionError('wrong shape ... {}'.format(x.shape))
 
     if save:
         cl.save(path='/Users/pedrocotovio/Desktop/', single_file=True)
 
+    if ifreturn:
+        return x, y
+
 
 if __name__ == '__main__':
-    test()
+    testcli()
