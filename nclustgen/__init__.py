6fba40a7c4664bc3270d841c0ea8b1c08b4f02bc
pedro <pgcotovio@gmail.com>
2021-06-04 00:36:54 +0100

    prepare packaging

--- nclustgen/__init__.py
+++ nclustgen/__init__.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__      = "Pedro Cotovio"
+__license__     = 'GNU GPLv3'
+__version__ = "1.0.0"
+
+
 from .BiclusterGen import BiclusterGenerator
 from .BiclusterGen import BiclusterGeneratorbyConfig
 from .TriclusterGen import TriclusterGenerator
