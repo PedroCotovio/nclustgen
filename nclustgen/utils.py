ea0061fb33e66a67a2a806e1c250f0b2bf60903d
Pedro Cotovio <pedro.cotovio@fraunhofer.pt>
2021-06-04 22:29:10 +0100

    [debug] linux float error

--- nclustgen/utils.py
+++ nclustgen/utils.py
@@ -4,7 +4,7 @@ import numpy as np
 def tensor_value_check(value):
 
     try:
-        return float(value)
+        return float(value.replace(",", "."))
 
     except ValueError:
         return np.nan
